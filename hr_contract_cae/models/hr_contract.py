1182c4e48264f0ce950ef20f302990852ac85f8d
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-02-05 15:47:11 +0100

    [ADD] hr_contract: attachements

--- hr_contract_cae/models/hr_contract.py
+++ hr_contract_cae/models/hr_contract.py
@@ -1,7 +1,9 @@
 from dateutil.relativedelta import relativedelta
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
+
+# _
+# from odoo.exceptions import ValidationError
 
 
 class ContractType(models.Model):
@@ -10,7 +12,7 @@ class ContractType(models.Model):
 
     echelon = fields.Selection(
         [("main", "Main"), ("amendment", "Amendment")],
-        string="Payment Mode",
+        string="Echelon",
         default="main",
     )
     max_usage = fields.Integer(string="Maximum Usage", default=False)
@@ -36,8 +38,9 @@ class Contract(models.Model):
         compute="_compute_type_echelon",
     )  # Todo: add translation "Echelone du Type de Contrat"
     contract_type_count = fields.Integer(
-        string="Contract Type Count", compute="_inital_to_latest"
+        string="Contract Type Count", compute="_compute_info_inital_to_latest"
     )
+
     contract_amendment_index = fields.Integer(
         string="Contract Amendment Index",
         help="'O' for main contract, '1' for it's first amendment, etc.",
@@ -63,7 +66,7 @@ class Contract(models.Model):
     latest_contract_id = fields.Many2one(
         comodel_name="hr.contract",
         string="Latest Contract",
-        compute="_inital_to_latest",
+        compute="_compute_info_inital_to_latest",
     )
 
     date_signature = fields.Date(
@@ -89,13 +92,30 @@ class Contract(models.Model):
     )
     notes = fields.Text(copy=False)
 
+    attachment_number = fields.Integer(
+        compute="_compute_attachment_number", string="Number of Attachments"
+    )
+    attachment_ids = fields.One2many(
+        comodel_name="ir.attachment",
+        inverse_name="res_id",
+        string="Attachments",
+        domain=[("res_model", "=", "hc.contract")],
+    )
+
     @api.model
     def create(self, vals):
-        # #  Todo: fix error when accessing self.contract_type_count here: "Expected singleton". Maybe because the copy function is accessed both from current and initial contract?
-        # if self.type_id.max_usage and self.contract_type_count > self.type_id.max_usage:
+        # #  Todo: fix error when accessing self.contract_type_count
+        # #  "Expected singleton".
+        # #  Maybe because the copy function is accessed both
+        # #  from current and initial contract?
+        # if (
+        #     self.type_id.max_usage
+        #     and self.contract_type_count > self.type_id.max_usage
+        # ):
         #     raise ValidationError(
         #         _(
-        #             "The maximum amount of %s contracts of type '%s' has been reached"
+        #             "The maximum amount of %s contracts "
+        #             "of type '%s' has been reached"
         #             % (self.contract_type_count, self.type_id)
         #         )
         #     )
@@ -165,7 +185,7 @@ class Contract(models.Model):
                 .id,
                 "initial_contract_id": latest_contract_id.initial_contract_id.id,
                 "parent_contract_id": latest_contract_id.id,
-                "contract_amendment_index": latest_contract_id.contract_amendment_index
+                "contract_amendment_index": latest_contract_id.contract_amendment_index  # noqa
                 + 1,
             }
         )
@@ -182,7 +202,7 @@ class Contract(models.Model):
 
     @api.multi
     @api.onchange("initial_contract_id", "type_id")
-    def _inital_to_latest(self):
+    def _compute_info_inital_to_latest(self):
         self.ensure_one()
         current_contract_id = self.initial_contract_id
         contract_type_count = current_contract_id.type_id == self.type_id
@@ -193,3 +213,30 @@ class Contract(models.Model):
             current_contract_id = current_contract_id.child_contract_id
         self.latest_contract_id = current_contract_id
         self.contract_type_count = contract_type_count
+
+    @api.multi
+    def _compute_attachment_number(self):
+        read_group_res = self.env["ir.attachment"].read_group(
+            [("res_model", "=", "hr.contract"), ("res_id", "in", self.ids)],
+            ["res_id"],
+            ["res_id"],
+        )
+        attach_data = {
+            res["res_id"]: res["res_id_count"] for res in read_group_res
+        }
+        for contract in self:
+            contract.attachment_number = attach_data.get(contract.id, 0)
+
+    @api.multi
+    def action_get_attachment_tree_view(self):
+        self.ensure_one()
+        attachment_action = self.env.ref("base.action_attachment")
+        action = attachment_action.read()[0]
+        action["context"] = {
+            "default_res_model": self._name,
+            "default_res_id": self.ids[0],
+        }
+        action["domain"] = str(
+            ["&", ("res_model", "=", self._name), ("res_id", "in", self.ids)]
+        )
+        return action
