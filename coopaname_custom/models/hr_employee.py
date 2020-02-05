08a1510e7ddb2e9460d540d4241886c89e8ac6d7
robin.keunen <robin@keunen.net>
2020-02-05 17:19:54 +0100

    [REM] coopaname_custom: identification_id generation

--- coopaname_custom/models/hr_employee.py
+++ coopaname_custom/models/hr_employee.py
@@ -7,8 +7,7 @@ import logging
 
 import phonenumbers
 
-from odoo import _, api, models
-from odoo.exceptions import ValidationError
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
@@ -28,43 +27,6 @@ def _format_phone_number(number):
 class Employee(models.Model):
     _inherit = "hr.employee"
 
-    _sql_constraints = [
-        (
-            "identification_id_uniq",
-            "unique(identification_id)",
-            _(
-                "The Employee Identification Number must "
-                "be unique across the company(s)."
-            ),
-        )
-    ]
-
-    @api.model
-    def _generate_identification_id(self, firstname, lastname):
-        try:
-            assert firstname
-            assert lastname
-        except AssertionError:
-            raise ValidationError(
-                _(
-                    "First name and last name are required to generate "
-                    "Identification No "
-                )
-            )
-
-        firstname_initial = firstname[0]
-        if len(lastname.split()) > 1:
-            lastnames = lastname.split()
-            lastname_initials = lastnames[0][0] + lastnames[1][0]
-        else:
-            lastname_initials = lastname[0]
-
-        initials = (firstname_initial + lastname_initials).upper()
-        seq_number = self.env["ir.sequence"].next_by_code(
-            "hr.employee.identification_id"
-        )
-        return "{}{}".format(initials, seq_number)
-
     @api.model
     def create(self, values):
 
@@ -74,10 +36,6 @@ class Employee(models.Model):
             values["work_phone"] = _format_phone_number(values["work_phone"])
 
         employee = super().create(values)
-        if not employee.identification_id:
-            employee.identification_id = self._generate_identification_id(
-                employee.firstname, employee.lastname
-            )
         return employee
 
     @api.multi
--- coopaname_custom/readme/DESCRIPTION.rst
+++ coopaname_custom/readme/DESCRIPTION.rst
@@ -2,5 +2,4 @@ Customization specific to Coopaname use case.
 
 * hr.applicant: copy name from partner_id to hr.applicant name and partner_name
 * hr.applicant: hide name from form view
-* hr.employee : add identification_id generation
 * hr.employee : format mobile and work phone
