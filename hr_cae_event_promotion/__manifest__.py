3c8845d119540bcf2f4ae4eddf9c2a6880811aa1
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-03-10 15:16:41 +0100

    [ADD] hr_cae_event_promotion

---
+++ hr_cae_event_promotion/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Manuel Claeys Bouuaert <manuel@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "HR CAE Event Promotions",
+    "summary": "Adds field and button to standard events to link an event "
+    "to a promotion and invite all it's employees at once.",
+    "author": "Coop IT Easy SCRLfs",
+    "website": "https://coopiteasy.be",
+    "category": "Human Resources",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["hr_cae_event", "hr_cae_promotion"],
+    "data": ["views/event.xml"],
+    "demo": [],
+    "installable": True,
+    "application": False,
+}
---
+++ hr_cae_event_promotion/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Adds field and button to standard events to link an event to a promotion and invite all it's employees at once.
