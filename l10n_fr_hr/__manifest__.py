42c01a90f505b5dc0cb2f72345ade371f0cd1244
Quentin Dupont <perso@quentindupont.fr>
2020-04-23 16:49:45 +0200

    Blackify

--- l10n_fr_hr/__manifest__.py
+++ l10n_fr_hr/__manifest__.py
@@ -3,19 +3,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'French Localization for HR',
-    'summary': 'French Localization for HR',
-    'version': '12.0.1.0.0',
-    'category': 'French Localization',
-    'author': 'GRAP,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'depends': [
-        'hr_cae',
-        'l10n_fr_department',
-    ],
-    "data": [
-        "views/hr_employee.xml",
-    ],
-    'installable': True,
-    'auto_install': True,
+    "name": "French Localization for HR",
+    "summary": "French Localization for HR",
+    "version": "12.0.1.0.0",
+    "category": "French Localization",
+    "author": "GRAP,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["hr_cae", "l10n_fr_department"],
+    "data": ["views/hr_employee.xml"],
+    "installable": True,
+    "auto_install": True,
 }
--- l10n_fr_hr/models/hr_employee.py
+++ l10n_fr_hr/models/hr_employee.py
@@ -5,7 +5,7 @@
 #   Quentin DUPONT <quentin.dupont@grap.coop>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class Employee(models.Model):
