7c8e18b768b82949f4407a76b9fd9959fa6e88b3
Quentin Dupont <perso@quentindupont.fr>
2020-08-13 10:44:36 +0200

    [IMP] Summary page on employee, disabled status, handle status origin

--- hr_grap_custom/models/hr_employee.py
+++ hr_grap_custom/models/hr_employee.py
@@ -2,7 +2,7 @@
 # Quentin DUPONT <quentin.dupont@grap.coop>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Employee(models.Model):
@@ -24,3 +24,21 @@ class Employee(models.Model):
     mutual_insurance_proof_received = fields.Boolean(
         string="Proof received", default=False
     )
+
+    show_status_origin = fields.Boolean(
+        string="Show Status Origin", default=False
+    )
+
+    disabled_worker_status = fields.Boolean(
+        string="Disabled Worker Status", default=False
+    )
+
+    disabled_worker_attachment = fields.Boolean(
+        string="Disabled Worker Attachment", default=False
+    )
+
+    # compute section
+    @api.onchange("disabled_worker_status")
+    def onchange_disabled_worker_status(self):
+        if self.disabled_worker_status is False:
+            self.disabled_worker_attachment = False
